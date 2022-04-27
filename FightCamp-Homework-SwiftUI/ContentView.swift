407cf763d963f823a63d3e6359f74f43c9e0f36b
David Troupe <dtroupe@Davids-MacBook-Pro.local>
2022-04-27 16:36:58 -0700

    split into two targets

---
+++ FightCamp-Homework-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+import SwiftUI
+
+// MARK: - ContentView
+
+struct ContentView: View {
+
+    var body: some View {
+
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+// MARK: - ContentView_Previews
+
+struct ContentView_Previews: PreviewProvider {
+
+    static var previews: some View {
+
+        ContentView()
+    }
+}
--- FightCamp-Homework-UIKit/SceneDelegate.swift
+++ FightCamp-Homework-UIKit/SceneDelegate.swift
@@ -12,7 +12,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         window = .init(windowScene: windowScene)
-        window?.rootViewController = nil // instantiate the view controller here
+        window?.rootViewController = HelloViewController() // instantiate the view controller here
         window?.makeKeyAndVisible()
+    }
+}
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
     }
 }
